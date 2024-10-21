5f62156b4135288812c185c4c08ff0bf6a6d9e4c
Helin Varoğlu <129685394+helinvaroglu@users.noreply.github.com>
2024-10-21 14:56:17 +0300

    Revert "Helin"

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './reset.css';
-// importing chakra ui
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <App />
   </React.StrictMode>
 );
 
