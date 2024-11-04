b3faf2deac45a67dd34629b94ec26c760d097c8c
helinvaroglu <helinv@sabanciuniv.edu>
2024-11-04 20:59:15 +0300

    Updated checking if a user is signed up

--- backend/src/users/user.model.js
+++ backend/src/users/user.model.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     fullName: {type: String, require: true},
@@ -9,6 +10,20 @@ const userSchema = new Schema({
         type: String, default: 'user'
     },
     phoneNumber: {type: String}
+})
+
+// compare passwords
+userSchema.methods.comparePassword = function (currPassword) {
+    return bcrypt.compare(currPassword, this.password);
+}
+
+// hash passwords to ensure the security
+userSchema.pre('save', async function(next) {
+    const user = this;
+    if(!user.isModified('password')) return next();
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+    next();
 })
 
 const User = new model('User', userSchema);
--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -17,4 +17,27 @@ router.post("/signup", async (req, res) => {
     }
 })
 
+// login endpoint
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+    
+    try {
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(404).send({message: 'This user is not signed up.'})
+        }
+        const isMatch = await user.comparePassword(password);
+
+        if(!isMatch) {
+            return res.status(401).send({message: 'Wrong password!'})
+        }
+
+        res.status(200).send({message: "Logged in successfully!", user});
+    
+    } catch (error) {
+        console.error("Error occurred while logging in.", error);
+        res.status(500).send({message: "Error occurred while logging in."})
+    }
+})
+
 module.exports = router;
