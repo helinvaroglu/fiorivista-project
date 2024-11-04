0ca0a901426c4a414b401a1ad216639316eddf2b
helinvaroglu <helinv@sabanciuniv.edu>
2024-11-04 17:52:58 +0300

    Connected database and created middleware

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+var cors = require('cors');
+const app = express();
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('FioriVista server is running.')
-})
+// main backend file of the project
+
+// setting middleware
+app.use(express.json({limit: "25mb"}));
+app.use((express.urlencoded({limit: "25mb"})));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+
+// connecting project with database
+main()
+    .then(() => console.log("Database is successfully connected."))
+    .catch(err => console.log(err));
+
+// name: admin password: fioriadmin123!
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+
+    // connecting express js for backend
+    app.get('/', (req, res) => {
+        res.send('FioriVista server is running.')
+    })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
