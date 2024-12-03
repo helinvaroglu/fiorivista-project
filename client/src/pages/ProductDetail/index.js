a8e3cfb304ec5c46c193d2a9e3ee1667bcc60836
helinvaroglu <helinv@sabanciuniv.edu>
2024-12-04 00:32:05 +0300

    Update index.js

--- client/src/pages/ProductDetail/index.js
+++ client/src/pages/ProductDetail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Text, Button, Image} from '@chakra-ui/react';
+import {Box, Text, Button, Image, VStack} from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { Grid, GridItem  } from '@chakra-ui/react';
 
@@ -13,29 +13,35 @@ function ProductDetail() {
     }
 
     return (
-        <Box m={5} bg="#ADBBDA" p={4} borderRadius="md" boxShadow="md">
-            <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-                <GridItem colSpan={1}>
-                    <Image src={product.image}  alt="product" />
-                </GridItem>
-                <GridItem colSpan={1} display="flex" flexDirection="column" alignItems="end">
-                    <Text fontWeight="bold" fontSize="2xl" color="#3D52A0" mb={1}>
-                        {product.name}
-                    </Text>
-                    <Text fontSize="xl" color="#F5D158" mb={2}>
-                        {product.rating}
-                    </Text>
-                    <Text fontSize="md" color="#323232" mb={10}>
-                        {product.description}
-                    </Text>
-                    <Text fontSize="xl" fontWeight="bold" color="#3D52A0" mb={4}>
-                        {product.price}
-                    </Text>
-                    <Button bg="#7091E6" color="#FDFDFF" size="lg">
-                        Order
-                    </Button>
-                </GridItem>
-            </Grid>
+        <Box 
+        display="flex"
+        justifyContent="center"
+        minHeight="80vh"
+      >
+            <Box m={5} bg="#ADBBDA" p={4} borderRadius="md" boxShadow="md" >
+                <Grid templateColumns="repeat(2, 1fr)" gap={6} alignItems="center">
+                    <GridItem colSpan={1} display="flex" alignItems="center"  h="100%">
+                        <Image src={product.image}  alt="product" />
+                    </GridItem>
+                    <GridItem colSpan={1} display="flex" flexDirection="column" alignItems="end"  h="100%">
+                        <Text fontWeight="bold" fontSize="2xl" color="#3D52A0" mb={1}>
+                            {product.name}
+                        </Text>
+                        <Text fontSize="xl" color="#F5D158" mb={2}>
+                            {product.rating}
+                        </Text>
+                        <Text fontSize="md" color="#323232" mb={10}>
+                            {product.description}
+                        </Text>
+                        <Text fontSize="xl" fontWeight="bold" color="#3D52A0" mb={4}>
+                            {product.price}
+                        </Text>
+                        <Button bg="#7091E6" color="#FDFDFF" size="lg">
+                            Order
+                        </Button>
+                    </GridItem>
+                </Grid>
+            </Box>
         </Box>
     );
 };
