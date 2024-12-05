48cb6b81dd471a584f4fbb0e8aaa9263ceee14be
helinvaroglu <helinv@sabanciuniv.edu>
2024-12-05 09:33:24 +0300

    Connected frontend and backend for catalog page

--- client/src/pages/Catalog/index.js
+++ client/src/pages/Catalog/index.js
@@ -3,11 +3,33 @@ import { Grid } from '@chakra-ui/react';
 
 import Card from '../../components/Card';
 import FilterBar from '../../components/FilterBar';
-import products from '../../data/products.json'
+import { useFetchAllProductsQuery } from '../../redux/features/Products/productApi';
+
 
 
 function Catalog() {
-  const [listProducts, setListProducts] = useState();
+  const [filtersState, setFiltersState] = useState({
+    flowerType: "all",
+    flowerType: "all", 
+    designType: "all", 
+    occasion: ""
+  })
+  const [currentPage, setCurrentPage] = useState(1);
+  const [ProductsPerPage] = useState(8);
+  const { flowerType, designType, occasion, price} = filtersState;
+
+  const {data: {products = [], totalPages, totalProducts} = {}, error, isLoading} = useFetchAllProductsQuery({
+    flowerType: flowerType, 
+    designType: designType, 
+    occasion: occasion, 
+    price: price, 
+    page: currentPage, 
+    limit: ProductsPerPage,
+  })
+
+  if(isLoading) return <div>Loading...</div>
+  if(error) return <div>Error: {error.message}</div>
+
 
   return (
     <div>
