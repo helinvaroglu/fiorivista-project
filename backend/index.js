804324570799dc9f769f7fbafb6cf53548f44c43
helinvaroglu <helinv@sabanciuniv.edu>
2024-12-25 20:30:51 +0300

    Added order route

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,12 @@ app.use(cors({
 // routes
 const authRoutes = require('./src/users/user.route');
 const productRoutes = require('./src/products/products.route');
+const orderRoutes = require("./src/orders/orders.route");
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
 
 
 // connecting project with database
--- backend/src/orders/orders.route.js
+++ backend/src/orders/orders.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const Order = require("./orders.model");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // checkout session
 router.post("/create-checkout-session", async (req, res) => {
@@ -28,8 +30,44 @@ router.post("/create-checkout-session", async (req, res) => {
         res.json({id: session.id})
 
     } catch (error) {
-        console.error("Error creating checkout", error);
+        console.error("Error creating checkout:", error);
         res.status(500).send({message: "Failed to create checkout"});
+    }
+});
+
+// confirm payment
+router.post("/confirm-payment", async (req, res) => {
+    const {session_id} = req.body;
+    try {
+        const session = await stripe.checkout.sessions.retrieve(session_id, {
+            expand: ["line_items", "payment_intent"]
+        });
+
+        const paymentIntentId = session.payment_intent.id;
+        let order = await Order.findOne({orderId: paymentIntentId});
+
+        if(!order) {
+            const lineItems = session.line_items.data.map((item) => ({
+
+            }));
+
+            const amount = session.amount_total / 100;
+            order = new Order({
+                orderId: paymentIntentId,
+                amount,
+                products: lineItems,
+                email: session.customer_details.email,
+                status: session.payment_intent.status === "succeed" ? "pending" : "failed"
+            })
+        } else {
+            order.status = session.payment_intent.status === "succeed" ? "pending" : "failed";
+        }
+        await order.save();
+        res.json({order});
+        
+    } catch (error) {
+        console.error("Error confirming payment:", error);
+        res.status(500).json({error: "Failed to confirm payment"});
     }
 })
 
